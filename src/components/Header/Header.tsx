e2cb8b2114ea80d42aedf78901bffc01a130b6bb
TalisMan701 <boss.kompaneec@mail.ru>
2022-11-18 17:44:31 +0500

    init components and install sass

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import classes from './Header.module.scss'
+
+const Header: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/TabBar/TabBar.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import classes from './TabBar.module.scss'
+
+const TabBar: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default TabBar;
